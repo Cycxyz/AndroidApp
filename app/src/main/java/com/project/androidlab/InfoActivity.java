5e64e4cda1a4a573b3a2e3c922c5686cae510be9
vladyslav.burtsevych <vladm2323@gmail.com>
2022-10-16 19:41:04 +0300

    add: info page content

--- app/src/main/java/com/project/androidlab/InfoActivity.java
+++ app/src/main/java/com/project/androidlab/InfoActivity.java
@@ -2,7 +2,9 @@ package com.project.androidlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void goBack(View view)
+    {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
