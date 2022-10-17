5e6107d2cc903ddffc7e43382a0c684b0996f5ed
vladyslav.burtsevych <vladm2323@gmail.com>
2022-10-17 09:15:07 +0300

    Add: text views, good Function type send to another activity

--- app/src/main/java/com/project/androidlab/CoefficientsActivity.java
+++ app/src/main/java/com/project/androidlab/CoefficientsActivity.java
@@ -1,18 +1,46 @@
 package com.project.androidlab;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class CoefficientsActivity extends AppCompatActivity {
+    Optional<FunctionType> cachedType;
 
-    FunctionType type = FunctionType.POWER;
+    private String getFunctionName()
+    {
+        int stringId = R.string.expFunc;
+        if (cachedType.isPresent() && cachedType.get() == FunctionType.POWER)
+        {
+            stringId = R.string.powerFunc;
+        }
+        return getString(stringId);
+    }
+
+    private void setFunctionLabel(String str)
+    {
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText(str);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coefficients);
         Bundle extras = getIntent().getExtras();
-        type = (FunctionType)extras.get(Globals.FUNCTION_TYPE_NAME);
+        if (extras != null)
+        {
+            cachedType = Optional.of((FunctionType) extras.get(Globals.FUNCTION_TYPE_NAME));
+        }
+        setFunctionLabel(getFunctionName());
     }
 }
